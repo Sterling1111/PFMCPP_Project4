14d1ea3aaad0a4595983d341427e76958ec9e42d
matkatmusic <matkatmusic@gmail.com>
2023-11-03 21:19:34 +0000

    Restored original instructions

--- main.cpp
+++ main.cpp
@@ -3,43 +3,65 @@ Project 4: Part 5 / 9
  video: Chapter 5 Part 2
  Operator Overloading.
 
-Do not delete your previous main.
+Do not delete your previous main. 
 
  Create a branch named Part5
 
-    Build/Run often with this task to make sure you're not breaking the code
-with each step. I recommend committing after you get each step working so you
-can revert to a working version easily if needed.
+    Build/Run often with this task to make sure you're not breaking the code with each step.
+    I recommend committing after you get each step working so you can revert to a working version easily if needed.
 
- 1) replace the add/subtract/multiply/etc functions in your UDTs with overloaded
-math operators e.g. add() would become operator+=() because it modifies the
-internal heap-allocated object. The easiest way to do this is to just rename
-your member functions. Do not: delete your member functions and add new ones.
-Simply rename them You do not need to change the Point class's multiply()
-functions.
+ 1) replace the add/subtract/multiply/etc functions in your UDTs with overloaded math operators 
+        e.g. add() would become operator+=() because it modifies the internal heap-allocated object.
+        The easiest way to do this is to just rename your member functions.  
+        Do not: delete your member functions and add new ones.  Simply rename them
+        You do not need to change the Point class's multiply() functions.
 
  2) Your overloaded operators should only take primitives, passed by value.
     since they are passed by value, they do not need to be const.
 
  3) don't delete your conversion functions.
 
- 4) your main() function should be the same as Project 4 part 4,
+ 4) your main() function should be the same as Project 4 part 4, 
     excluding the changes made due to 1)
 
- 5) delete the example below after it makes sense how your code will change due
-to 1).
+ 5) delete the example below after it makes sense how your code will change due to 1).
+ */
+
+#include <iostream>
+namespace Example
+{
+    int main()
+    {
+        FloatType floatNum(4.3f);
+        IntType intNum(2);
+        IntType intNum2(6);
+
+        /* 
+        if you previously had a line like this demonstrating chaining:
+
+            intNum.add(3).add(4.5f).divide(floatNum); 
+
+        it should become:
+        */
+        intNum += 3;
+        intNum += 4.5f;
+        intNum /= floatNum;
+        std::cout << "intNum: " << intNum << std::endl;
+
+        return 0;
+    }
+}
+
+ /*
+ 6) compile/link/run to make sure you don't have any errors or warnings.
+
+ 7) your program should produce the exact same output as Project 4 part 4, listed below.
+    use https://www.diffchecker.com/diff to make sure it is the same.
  */
 
 /*
-6) compile/link/run to make sure you don't have any errors or warnings.
-
-7) your program should produce the exact same output as Project 4 part 4, listed
-below. use https://www.diffchecker.com/diff to make sure it is the same.
-*/
-
-/*
 your program should generate the following output EXACTLY.
-This includes any warnings included below.
+This includes any warnings included below.  
 
 The output should have zero warnings.
 
@@ -65,42 +87,44 @@ New value of ft = (ft + 3.0f) * 1.5f / 5.0f = 0.975
 
 Initial value of dt: 0.8
 Initial value of it: 590
-Use of function concatenation (mixed type arguments)
+Use of function concatenation (mixed type arguments) 
 New value of dt = (dt * it) / 5.0f + ft = 95.375
 ---------------------
 
-Intercept division by 0
-New value of it = it / 0 = error: integer division by zero is an error and will
-crash the program! 590 New value of ft = ft / 0 = warning: floating point
-division by zero! inf New value of dt = dt / 0 = warning: floating point
-division by zero! inf
+Intercept division by 0 
+New value of it = it / 0 = error: integer division by zero is an error and will crash the program!
+590
+New value of ft = ft / 0 = warning: floating point division by zero!
+inf
+New value of dt = dt / 0 = warning: floating point division by zero!
+inf
 ---------------------
 
 The result of FloatType^4 divided by IntType is: 26.9136
 The result of DoubleType times 3 plus IntType is : 67.3
-The result of IntType divided by 3.14 multiplied by DoubleType minus FloatType
-is: 711 An operation followed by attempts to divide by 0, which are ignored and
-warns user: error: integer division by zero is an error and will crash the
-program! error: integer division by zero is an error and will crash the program!
+The result of IntType divided by 3.14 multiplied by DoubleType minus FloatType is: 711
+An operation followed by attempts to divide by 0, which are ignored and warns user: 
+error: integer division by zero is an error and will crash the program!
+error: integer division by zero is an error and will crash the program!
 error: integer division by zero is an error and will crash the program!
 505521
 FloatType x IntType  =  13143546
 (IntType + DoubleType + FloatType) x 24 = 315447336
-Power tests with FloatType
+Power tests with FloatType 
 pow(ft1, floatExp) = 2^2 = 4
 pow(ft1, itExp) = 4^2 = 16
 pow(ft1, ftExp) = 16^2 = 256
 pow(ft1, dtExp) = 256^2 = 65536
 ---------------------
 
-Power tests with DoubleType
+Power tests with DoubleType 
 pow(dt1, doubleExp) = 2^2 = 4
 pow(dt1, itExp) = 4^2 = 16
 pow(dt1, ftExp) = 16^2 = 256
 pow(dt1, dtExp) = 256^2 = 65536
 ---------------------
 
-Power tests with IntType
+Power tests with IntType 
 pow(it1, intExp) = 2^2 = 4
 pow(it1, itExp) = 4^2 = 16
 pow(it1, ftExp) = 16^2 = 256
@@ -133,8 +157,9 @@ Point { x: 15, y: 20 }
 
 good to go!
 
-Use a service like https://www.diffchecker.com/diff to compare your output.
+Use a service like https://www.diffchecker.com/diff to compare your output. 
 */
+
 
 struct FloatType;
 struct DoubleType;
